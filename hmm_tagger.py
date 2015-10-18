787096667bd0d0326ef331fd11b1b72a0b584664
Frederik Stensaeth <stensaethf@carleton.edu>
2015-10-18 17:06:01 -0500

    viterbi basics setup

--- hmm_tagger.py
+++ hmm_tagger.py
@@ -1,4 +1,11 @@
 '''
+hmm_tagger.py
+Frederik Roenn Stensaeth
+10.18.15
+
+A Python program that uses the Viterbi algorithm and the model file that 
+was created to determine the most probable tag sequence given a sequence 
+of words/tokens.
 '''
 
 import sys
@@ -9,20 +16,65 @@ def printError():
 	"""
 	"""
 	print('Error.')
-	print('Usage: $ echo <string to be tagged> | python hmm_tagger.py [countmodel.dat | trainmodel.dat]')
-	sys.exit
+	print('Usage: $ echo <string to be tagged> | python hmm_tagger.py',
+	      '[countmodel.dat | trainmodel.dat]')
+	sys.exit()
 
-def viterbi(matrix_a, matrix_b, observations):
+def viterbi(matrix_a, matrix_b, obs):
 	"""
 	"""
-	Xx
+	# Create data structures for going back and forth between tokens in string
+	# form and integer form.
+	token_to_num = {}
+	token_to_num['<START>'] = 0
+	num_to_token = []
+	num_to_token.append('<START>')
+	for token in enumerate(matrix_a):
+		if token not in ['<START>', '<END>']:
+			num_to_token.append(token)
+			token_to_num[token] = len(num_to_token) - 1
+	num_to_token.append('<END>')
+	token_to_num['<END>'] = len(num_to_token) - 1
+
+	n = len(matrix_a)
+	T = len(obs)
+
+	# Setups the vit and backpoitner matrices that we will use for computing
+	# the most probable tag sequence and for keeping track of what tags that
+	# tag sequence actually consists of.
+	# [[0 for i in range(column)] for j in range(row)] 
+	vit = [[0 for i in range(T)] for j in range(n + 2)]
+	backpointer = [[0 for i in range(T)] for j in range(n + 2)]
+
+	# NOTE:
+	# From here on we need to be defensive. The words may not have been seen
+	# before, so need to alter the code beneath so that it can handle that.
+
+	for state in range(1, n):
+		s = num_to_token[state]
+		vit[state][0] = matrix_a['<START>'][s] * matrix_b[s][obs[0]]
+		backpointer[state][0] = 0
+
+	for t in range(2, T):
+		for state in range(1, n):
+			vit[state][t] = float('-inf')
+			s = num_to_token[state]
+			for sprev in range(1, n):
+				sp = num_to_token[sprev]
+				vtj = vit[sprev][t - 1] * matrix_a[sp][s] 
+										* matrix_b[s][obs[t]]
+				if vtj > vit[state][t]:
+					vit[state][t] = vtj
+					backpointer[state][t] = num_to_token[sprev]
+
+	# Find sequence of tags.
 
 	return None
 
-def hmmTagger(f, std_input):
+def hmmTagger(f, std_in_raw):
 	"""
 	"""
-	std_input = std_input.read().strip().split(' ')
+	std_input = std_in_raw.read().strip().split(' ')
 
 	model = pickle.load(open(f, 'rb'))
 	matrix_a = model['a']
@@ -33,10 +85,13 @@ def hmmTagger(f, std_input):
 	print(result)
 
 def main():
-	# if len(sys.argv) != 2:
-	# 	printError()
+	if len(sys.argv) != 2:
+		printError()
 
 	if not os.path.isfile(sys.argv[1]):
+		printError()
+
+	if sys.argv[1] not in ['countmodel.dat', 'trainmodel.dat']:
 		printError()
 
 	hmmTagger(sys.argv[1], sys.stdin)
