88c2349024bed5a780ba3d5369f47d53cba27059
Frederik Stensaeth <stensaethf@carleton.edu>
2015-10-18 14:28:44 -0500

    A and B should be working now

--- hmm_model_build.py
+++ hmm_model_build.py
@@ -11,7 +11,7 @@ def printError():
 	print('Usage: $ python3 hmm_model_build.py <file for building>')
 	sys.exit()
 
-def getTransitionBigrams(sent_list):
+def getTransitionCounts(sent_list):
 	"""
 	"""
 	model = {}
@@ -41,18 +41,92 @@ def getTransitionBigrams(sent_list):
 
 	return model
 
-def getEmissions(sent_list):
+def getTransitionProbabilities(transition_counts):
 	"""
 	"""
-	return None
+	matrix_a = {}
+	matrix_a[0] = transition_counts[0]
+
+	# Loops over and finds all the different bigrams. Calculates the
+	# probability of each and stores them in matrix_a.
+	for first in transition_counts:
+		for second in transition_counts[first]:
+			count_bi = transition_counts[first][second][0]
+			count_uni = transition_counts[first][0]
+
+			matrix_a[first] = {}
+			matrix_a[first][second] = (count_bi / count_uni)
+
+	return matrix_a
+
+def getTransitionMatrix(sent_list):
+	"""
+	"""
+	transition_counts = getTransitionCounts(sent_list)
+
+	matrix_a = getTransitionProbabilities(transition_counts)
+
+	return matrix_a
+
+def getEmissionCounts(sent_list):
+	"""
+	"""
+	model = {}
+	model[0] = 0
+
+	for sent_raw in sent_list:
+		sent = sent_raw.split(' ')
+		for i, word_tag in enumerate(sent):
+			word = word_tag.split('/')[0]
+			tag = word_tag.split('/')[1]
+
+			if tag in model:
+				model[tag][0] += 1
+			else:
+				model[tag] = {}
+				model[tag][0] = 1
+
+			if word in model[tag]:
+				model[tag][word][0] += 1
+			else:
+				model[tag][word] = {}
+				model[tag][word][0] = 1
+
+	return model
+
+def getEmissionProbabilities(emission_counts):
+	"""
+	"""
+	matrix_b = {}
+	matrix_b[0] = emission_counts[0]
+
+	for state in emission_counts:
+		for word in emission_counts[state]:
+			count_bi = emission_counts[state][word][0]
+			count_uni = emission_counts[state]
+
+			matrix_b[state] = {}
+			matrix_b[state][word] = (count_bi / count_uni)
+
+	return matrix_b
+
+def getEmissionMatrix(sent_list):
+	"""
+	b_i(O_t) = P(O_t | q_i)
+	"""
+	emission_counts = getEmissionCounts(sent_list)
+
+	matrix_b = getEmissionProbabilities(emission_counts)
+
+	return matrix_b
 
 def hmmBuilder(f):
 	"""
 	"""
 	sent_list = f.split('./.')
-	matrix_a = getTransitionBigrams(sent_list)
+	matrix_a = getTansistionMatrix(sent_list)
 
-	matrix_b = getEmissionBigrams(sent_list)
+	matrix_b = getEmissionMatrix(sent_list)
 
 	model = {}
 	model['a'] = matrix_a
