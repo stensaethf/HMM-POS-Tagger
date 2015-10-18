6fb3d8e9911eb27ddf46ec91882be717bcf514e6
Frederik Stensaeth <stensaethf@carleton.edu>
2015-10-18 16:06:49 -0500

    setup of tagger

--- hmm_tagger.py
+++ hmm_tagger.py
@@ -12,20 +12,23 @@ def printError():
 	print('Usage: $ echo <string to be tagged> | python hmm_tagger.py [countmodel.dat | trainmodel.dat]')
 	sys.exit
 
-def hmmTagger(f, st_input):
+def viterbi(matrix_a, matrix_b, observations):
 	"""
 	"""
-	result = st_input.strip().split(' ')
+	Xx
+
+	return None
+
+def hmmTagger(f, std_input):
+	"""
+	"""
+	std_input = std_input.read().strip().split(' ')
 
 	model = pickle.load(open(f, 'rb'))
-	print(model['a'])
-	print()
-	print(model['b'])
+	matrix_a = model['a']
+	matrix_b = model['b']
 
-
-
-
-	# result = ' '.join(viterbi(Xx))
+	result = ' '.join(viterbi(matrix_a, matrix_b, std_input))
 
 	print(result)
 
@@ -36,7 +39,7 @@ def main():
 	if not os.path.isfile(sys.argv[1]):
 		printError()
 
-	hmmTagger(sys.argv[1], sys.stdin.read())
+	hmmTagger(sys.argv[1], sys.stdin)
 
 
 if __name__ == '__main__':
